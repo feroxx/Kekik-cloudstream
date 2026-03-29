dae49e2c3d2625533db7411ab00f558a2075839d
feroxx <frhatfrat@gmail.com>
2026-03-29 23:40:05 +0300

    sinewix by Patr0n

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 rootProject.name = "CloudstreamPlugins"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
