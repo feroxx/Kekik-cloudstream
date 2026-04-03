325d9b37a271330c506ed3ad406405a2b1fc5591
feroxx <frhatfrat@gmail.com>
2026-04-04 01:47:47 +0300

    Update DiziPalModels.kt

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalModels.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalModels.kt
@@ -8,31 +8,31 @@ import com.fasterxml.jackson.annotation.JsonProperty
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class SearchItem(
     @JsonProperty("object_id") 
-    val id: Int? = null,
+    val id: Int,
     
     @JsonProperty("object_name") 
-    val title: String? = null,
+    val title: String,
     
     @JsonProperty("object_alternative_name") 
-    val trTitle: String? = null,
+    val trTitle: String,
     
     @JsonProperty("object_poster_url") 
-    val poster: String? = null,
+    val poster: String,
     
     // JSON'da hem object_categories hem de object_logo_url (saçma bir şekilde) kategori içeriyor
     @JsonProperty("object_categories") 
-    val genres: String? = null,
+    val genres: String,
     
     @JsonProperty("object_related_imdb_point") 
-    val imdb: Double? = null,
+    val imdb: Double,
     
     @JsonProperty("object_release_year") 
-    val year: Int? = null,
+    val year: Int,
     
     @JsonProperty("used_type") 
-    val type: String? = null,
+    val type: String,
     
     // Asıl linki oluşturacağımız parçacık
     @JsonProperty("used_slug") 
-    val slug: String? = null
+    val slug: String
 )
