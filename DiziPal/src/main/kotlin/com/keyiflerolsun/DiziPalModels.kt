a98555f9f20e632e4427b9d7cdf9d82c8edd8682
feroxx <frhatfrat@gmail.com>
2026-04-04 01:41:03 +0300

    Update DiziPalModels.kt

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalModels.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalModels.kt
@@ -2,19 +2,37 @@
 
 package com.keyiflerolsun
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class SearchItem(
-    @JsonProperty("id") val id: String,
-    @JsonProperty("title") val title: String,
-    @JsonProperty("tr_title") val trTitle: String,
-    @JsonProperty("poster") val poster: String,
-    @JsonProperty("genres") val genres: String,
-    @JsonProperty("imdb") val imdb: String,
-    @JsonProperty("duration") val duration: String,
-    @JsonProperty("year") val year: String,
-    @JsonProperty("view") val view: Int,
-    @JsonProperty("type") val type: String = "defaultType",
-    @JsonProperty("url") val url: String
+    @JsonProperty("object_id") 
+    val id: Int? = null,
+    
+    @JsonProperty("object_name") 
+    val title: String? = null,
+    
+    @JsonProperty("object_alternative_name") 
+    val trTitle: String? = null,
+    
+    @JsonProperty("object_poster_url") 
+    val poster: String? = null,
+    
+    // JSON'da hem object_categories hem de object_logo_url (saçma bir şekilde) kategori içeriyor
+    @JsonProperty("object_categories") 
+    val genres: String? = null,
+    
+    @JsonProperty("object_related_imdb_point") 
+    val imdb: Double? = null,
+    
+    @JsonProperty("object_release_year") 
+    val year: Int? = null,
+    
+    @JsonProperty("used_type") 
+    val type: String? = null,
+    
+    // Asıl linki oluşturacağımız parçacık
+    @JsonProperty("used_slug") 
+    val slug: String? = null
 )
