1e7fceff3146cbfcac4e0ed61db4e996a7eb98e8
feroxx <frhatfrat@gmail.com>
2026-04-07 14:08:35 +0300

    dzp

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
@@ -23,7 +23,7 @@ import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
 class DiziPal : MainAPI() {
-    override var mainUrl              = "https://dizipal1545.com"
+    override var mainUrl              = "https://dizipal1546.com"
     override var name                 = "DiziPal"
     override val hasMainPage          = true
     override var lang                 = "tr"
