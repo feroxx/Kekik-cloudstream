d95ccd0882254115c8787874fa36d6e98819ef80
feroxx <frhatfrat@gmail.com>
2026-05-16 19:25:05 +0300

    Update mainUrl to new domain

--- DiziPalOriginal/src/main/kotlin/com/keyiflerolsun/DiziPalOriginal.kt
+++ DiziPalOriginal/src/main/kotlin/com/keyiflerolsun/DiziPalOriginal.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 
 class DiziPalOriginal : MainAPI() {
-    override var mainUrl              = "https://dizipal2069.com"
+    override var mainUrl              = "https://dizipal2071.com"
     override var name                 = "DiziPalOriginal"
     override val hasMainPage          = true
     override var lang                 = "tr"
